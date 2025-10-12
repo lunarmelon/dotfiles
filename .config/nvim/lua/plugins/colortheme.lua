34b758516b212a05ef837b9cdb305cd0968e4ddc
lunarmelon <lunarmelon@tuta.io>
2025-10-12 14:06:13 -0600

    chore(nvim): move tabline colors to mini config

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -4,36 +4,5 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd("colorscheme catppuccin-mocha")
-
-		vim.api.nvim_set_hl(0, "MiniTablineCurrent", {
-			fg = "#89b4fa",
-			bg = "NONE",
-			bold = true,
-		})
-		vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", {
-			fg = "#f9e2af",
-			bg = "NONE",
-			bold = true,
-		})
-		vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", {
-			fg = "#f9e2af",
-			bg = "NONE",
-			bold = false,
-		})
-		vim.api.nvim_set_hl(0, "MiniTablineVisible", {
-			fg = "#89b4fa",
-			bg = "NONE",
-			bold = false,
-		})
-		vim.api.nvim_set_hl(0, "MiniTablineHidden", {
-			fg = "#6c7086",
-			bg = "NONE",
-			bold = false,
-		})
-		vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", {
-			fg = "#7e7256",
-			bg = "NONE",
-			bold = false,
-		})
 	end,
 }
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -10,6 +10,38 @@ return {
 		local minisurround = require("mini.surround")
 		local minitabline = require("mini.tabline")
 
+		-- Mini tabline colors
+		vim.api.nvim_set_hl(0, "MiniTablineCurrent", {
+			fg = "#89b4fa",
+			bg = "NONE",
+			bold = true,
+		})
+		vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", {
+			fg = "#f9e2af",
+			bg = "NONE",
+			bold = true,
+		})
+		vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", {
+			fg = "#f9e2af",
+			bg = "NONE",
+			bold = false,
+		})
+		vim.api.nvim_set_hl(0, "MiniTablineVisible", {
+			fg = "#89b4fa",
+			bg = "NONE",
+			bold = false,
+		})
+		vim.api.nvim_set_hl(0, "MiniTablineHidden", {
+			fg = "#6c7086",
+			bg = "NONE",
+			bold = false,
+		})
+		vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", {
+			fg = "#7e7256",
+			bg = "NONE",
+			bold = false,
+		})
+
 		miniclue.setup({
 			triggers = {
 				-- Leader triggers
