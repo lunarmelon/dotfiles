b0eb80a9c65eab601604fc19c39ca17ef3a694a7
lunarmelon <lunarmelon@tuta.io>
2026-02-13 23:22:00 -0600

    fix(nvim): typos

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -124,6 +124,8 @@ return {
 				},
 			},
 			cssls = {},
+			astro = {},
+			clangd = {},
 			emmet_ls = {
 				filetypes = {
 					"html",
@@ -187,7 +189,7 @@ return {
 			-- 1. A fresh empty table (to avoid mutating capabilities globally)
 			-- 2. Your capabilities object with Neovim + cmp features
 			-- 3. Any server-specific cfg.capabilities if defined in `servers`
-			cfg.capabilites = vim.tbl_extend("force", {}, capabilities, cfg.capabilites or {})
+			cfg.capabilities = vim.tbl_deep_extend("force", {}, capabilities, cfg.capabilities or {})
 
 			vim.lsp.config(server, cfg)
 			vim.lsp.enable(server)
