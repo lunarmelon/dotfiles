0eeb3b0d1548971e6edcf011714da43140238d12
lunarmelon <lunarmelon@tuta.io>
2026-02-22 19:51:56 -0600

    feat(emacs): add emacs config

---
+++ .config/emacs/init.el
@@ -0,0 +1,50 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(set-fringe-mode 10)
+
+(global-display-line-numbers-mode 1)
+
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font:antialias=subpixel" :height 140)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(Use-package catppuccin-theme
+    :ensure t
+    :init (load-theme 'catppuccin :noconfirm))
+
+;; Initialize packages sources
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(
+package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
