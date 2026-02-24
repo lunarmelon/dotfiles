5195e07ea4e1df1fe3898f0a6921cab0cd4438c7
lunarmelon <lunarmelon@tuta.io>
2026-02-24 17:34:45 -0600

    feat(nvim): add neowiki

---
+++ .config/nvim/lua/plugins/neowiki.lua
@@ -0,0 +1,13 @@
+return {
+	"echaya/neowiki.nvim",
+	opts = {
+		wiki_dirs = {
+			{ name = "Personal", path = "~/wiki" },
+		},
+	},
+	keys = {
+		{ "<leader>ww", "<cmd>lua require('neowiki').open_wiki()<cr>", desc = "Open Wiki" },
+		{ "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
+		{ "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = "Open Wiki in Tab" },
+	},
+}
