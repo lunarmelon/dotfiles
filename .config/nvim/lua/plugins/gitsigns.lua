103be9413b2c0104340c4d8040ca08e03b029c05
lunarmelon <lunarmelon@tuta.io>
2026-03-01 16:37:42 -0600

    chore(nvim): change gitsigns signs

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,18 +3,20 @@ return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
+			add = { text = "┃" },
+			change = { text = "┃" },
 			delete = { text = "_" },
 			topdelete = { text = "‾" },
 			changedelete = { text = "~" },
+			untracked = { text = "┆" },
 		},
 		signs_staged = {
-			add = { text = "+" },
-			change = { text = "~" },
+			add = { text = "┃" },
+			change = { text = "┃" },
 			delete = { text = "_" },
 			topdelete = { text = "‾" },
 			changedelete = { text = "~" },
+			untracked = { text = "┆" },
 		},
 	},
 }
