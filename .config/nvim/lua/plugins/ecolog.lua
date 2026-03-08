a87fb09959d32af42925abab62fdefebdb301519
lunarmelon <lunarmelon@tuta.io>
2026-03-08 15:08:50 -0600

    fix(nvim): fixed conflicting keybindings

--- .config/nvim/lua/plugins/ecolog.lua
+++ .config/nvim/lua/plugins/ecolog.lua
@@ -1,13 +1,12 @@
 return {
 	"ph1losof/ecolog.nvim",
 	keys = {
-		{ "<leader>e", "", desc = "+ecolog", mode = { "n", "v" } },
-		{ "<leader>el", "<Cmd>EcologShelterLinePeek<cr>", desc = "Peek line" },
+		{ "<leader>ec", "", desc = "+ecolog", mode = { "n", "v" } },
+		{ "<leader>elp", "<Cmd>EcologShelterLinePeek<cr>", desc = "Peek line" },
 		{ "<leader>ey", "<Cmd>EcologCopy<cr>", desc = "Copy value under cursor" },
 		{ "<leader>ei", "<Cmd>EcologInterpolationToggle<cr>", desc = "Toggle interpolation" },
 		{ "<leader>eh", "<Cmd>EcologShellToggle<cr>", desc = "Toggle shell variables" },
 		{ "<leader>ge", "<cmd>EcologGoto<cr>", desc = "Go to env file" },
-		{ "<leader>ec", "<cmd>EcologSnacks<cr>", desc = "Open a picker" },
 		{ "<leader>eS", "<cmd>EcologSelect<cr>", desc = "Switch env file" },
 		{ "<leader>es", "<cmd>EcologShelterToggle<cr>", desc = "Shelter toggle" },
 	},
