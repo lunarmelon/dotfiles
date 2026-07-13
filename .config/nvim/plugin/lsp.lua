a40b82710cf1505c841b979ecffe3ca43fe4d3b3
lunarmelon <lunarmelon@tuta.io>
2026-07-13 13:59:51 -0600

    feat(nvim): add lsps

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -22,64 +22,102 @@ vim.lsp.config("lua_ls", {
 	},
 })
 
-do
-	local selene = require("efmls-configs.linters.selene")
-	local stylua = require("efmls-configs.formatters.stylua")
-
-	local ruff_lint = require("efmls-configs.linters.ruff")
-	local ruff_format = require("efmls-configs.formatters.ruff")
-
-	local prettier_d = require("efmls-configs.formatters.prettier_d")
-	local eslint_d = require("efmls-configs.linters.eslint_d")
-
-	local fixjson = require("efmls-configs.formatters.fixjson")
-
-	local shellcheck = require("efmls-configs.linters.shellcheck")
-	local shfmt = require("efmls-configs.formatters.shfmt")
-
-	-- local cpplint = require("efmls-configs.linters.cpplint")
-	local clangfmt = require("efmls-configs.formatters.clang_format")
-
-	vim.lsp.config("efm", {
-		filetypes = {
-			"c",
-			"cpp",
-			"css",
-			"html",
-			"javascript",
-			"javascriptreact",
-			"json",
-			"jsonc",
-			"lua",
-			"markdown",
-			"python",
-			"sh",
-			"typescript",
-			"typescriptreact",
+vim.lsp.config("jsonls", {
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
 		},
-		init_options = { documentFormatting = true },
-		settings = {
-			languages = {
-				c = { clangfmt },
-				cpp = { clangfmt },
-				css = { prettier_d },
-				html = { prettier_d },
-				javascript = { eslint_d, prettier_d },
-				javascriptreact = { eslint_d, prettier_d },
-				json = { eslint_d, fixjson },
-				jsonc = { eslint_d, fixjson },
-				lua = { selene, stylua },
-				markdown = { prettier_d },
-				python = { ruff_lint, ruff_format },
-				sh = { shellcheck, shfmt },
-				typescript = { eslint_d, prettier_d },
-				typescriptreact = { eslint_d, prettier_d },
+	},
+})
+
+vim.lsp.config("pylsp", {
+	settings = {
+		pylsp = {
+			plugins = {
+				pyflakes = { enabled = false },
+				pycodestyle = { enabled = false },
+				autopep8 = { enabled = false },
+				yapf = { enabled = false },
+				mccabe = { enabled = false },
+				pylsp_mypy = { enabled = false },
+				pylsp_black = { enabled = false },
+				pylsp_isort = { enabled = false },
+				pylsp_rope = { enabled = false },
 			},
 		},
-	})
-end
+	},
+})
+
+local selene = require("efmls-configs.linters.selene")
+local stylua = require("efmls-configs.formatters.stylua")
+
+local ruff_lint = require("efmls-configs.linters.ruff")
+local ruff_format = require("efmls-configs.formatters.ruff")
+local ruff_sort = require("efmls-configs.formatters.ruff_sort")
+
+local prettier_d = require("efmls-configs.formatters.prettier_d")
+local eslint_d = require("efmls-configs.linters.eslint_d")
+local htmlhint = require("efmls-configs.linters.htmlhint")
+local biome_format = require("efmls-configs.formatters.biome")
+
+local fixjson = require("efmls-configs.formatters.fixjson")
+
+local shellcheck = require("efmls-configs.linters.shellcheck")
+local shfmt = require("efmls-configs.formatters.shfmt")
+
+-- local cpplint = require("efmls-configs.linters.cpplint")
+local clangfmt = require("efmls-configs.formatters.clang_format")
+
+vim.lsp.config("efm", {
+	filetypes = {
+		"c",
+		"cpp",
+		"css",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"jsonc",
+		"lua",
+		"markdown",
+		"python",
+		"sh",
+		"typescript",
+		"typescriptreact",
+	},
+	init_options = { documentFormatting = true },
+	settings = {
+		languages = {
+			c = { clangfmt },
+			cpp = { clangfmt },
+			css = { biome_format, eslint_d },
+			html = { biome_format, htmlhint },
+			javascript = { eslint_d, biome_format },
+			javascriptreact = { eslint_d, biome_format },
+			json = { eslint_d, fixjson },
+			jsonc = { eslint_d, fixjson },
+			lua = { selene, stylua },
+			markdown = { prettier_d },
+			python = { ruff_lint, ruff_format, ruff_sort },
+			sh = { shellcheck, shfmt },
+			typescript = { eslint_d, biome_format },
+			typescriptreact = { eslint_d, biome_format },
+			yaml = { prettier_d },
+		},
+	},
+})
 
 vim.lsp.enable({
-	"lua_ls",
+	"bashls",
+	"clangd",
+	"cssls",
 	"efm",
+	"emmet_ls",
+	"html",
+	"lua_ls",
+	"marksman",
+	"pylsp",
+	"ruff",
+	"ts_ls",
 })
